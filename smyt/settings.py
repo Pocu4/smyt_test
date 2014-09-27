b5c586f59a272dad20d124ffe3715ffd00cd0db6
Ailurus <rus@ailurus.ru>
2014-09-27 17:46:01 +0300

    Initial settings

--- smyt/settings.py
+++ smyt/settings.py
@@ -67,16 +67,16 @@ DATABASES = {
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
+#
+# LANGUAGE_CODE = 'en-us'
+#
+# TIME_ZONE = 'UTC'
+#
+# USE_I18N = True
+#
+# USE_L10N = True
+#
+# USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
